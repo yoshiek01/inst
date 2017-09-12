5092a97ed6ded41af6247053ffaf058f42aabd50
yoshiek01 <yoshie-k@nttpc.co.jp>
2017-09-12 03:43:57 +0000

    20170912commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,18 +4,24 @@ Rails.application.routes.draw do
     collection do
       post :confirm
     end
+
   end
-  
+
   resources :contacts,only: [:new, :create]do
     collection do
       post :confirm
     end
   end
-  
+
+
   root 'top#index'
   #ルートディレクトリはtopコントローラのindexアクション
-  
- #end
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+
+ end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -70,4 +76,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
