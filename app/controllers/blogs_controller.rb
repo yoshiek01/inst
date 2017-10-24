9ffaf6faa3fdab9504179c676836df3c017a3ebf
yoshiek01 <yoshie-k@nttpc.co.jp>
2017-10-24 10:45:24 +0000

    blogusernameエラー.20171024

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -16,7 +16,7 @@ class BlogsController < ApplicationController
 
  def create
   @blog =  Blog.new(blogs_params)
-
+  @blog.user_id = current_user.id
    if @blog.save
     redirect_to blogs_path ,notice: "ブログを作成しました！"
     NoticeMailer.sendmail_blog(@blog).deliver
